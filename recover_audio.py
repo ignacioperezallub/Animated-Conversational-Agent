928bc2ef495585be7f54d303cf11105333b8a104
Andres Mesa <a1mesama@enib.fr>
2023-03-15 15:12:55 +0100

    Recover audio from pc output

---
+++ recover_audio.py
@@ -0,0 +1,52 @@
+import pyaudio
+import wave
+
+p = pyaudio.PyAudio()
+
+index = p.get_device_count()
+device_name = "ADAT (7+8) (RME Fireface UCX)"
+
+for i in range(index):
+    device = p.get_device_info_by_index(i)
+    if(device["name"] == device_name and device["maxInputChannels"] > 0):
+        input_device_index = device["index"]
+        print(device)
+        break
+
+chunk = 1024  # Record in chunks of 1024 samples
+sample_format = pyaudio.paInt16  # 16 bits per sample
+channels = 1
+fs = 44100  # Record at 44100 samples per second
+seconds = 9
+filename = "output.wav"
+
+print('Recording')
+
+stream = p.open(format=sample_format,
+                channels=channels,
+                rate=fs,
+                frames_per_buffer=chunk,
+                input=True, input_device_index = device["index"])
+
+frames = []  # Initialize array to store frames
+
+# Store data in chunks for 3 seconds
+for i in range(0, int(fs / chunk * seconds)):
+    data = stream.read(chunk)
+    frames.append(data)
+
+# Stop and close the stream 
+stream.stop_stream()
+stream.close()
+# Terminate the PortAudio interface
+p.terminate()
+
+print('Finished recording')
+
+# Save the recorded data as a WAV file
+wf = wave.open(filename, 'wb')
+wf.setnchannels(channels)
+wf.setsampwidth(p.get_sample_size(sample_format))
+wf.setframerate(fs)
+wf.writeframes(b''.join(frames))
+wf.close()
