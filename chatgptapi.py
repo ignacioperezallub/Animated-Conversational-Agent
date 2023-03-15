cbb0e4d85da9c108b90253489cd05f75f0add18f
trajano080 <101326947+trajano080@users.noreply.github.com>
2023-03-15 15:03:39 +0100

    Chat GPT API question-answer working

---
+++ chatgptapi.py
@@ -0,0 +1,27 @@
+import openai
+
+openai.api_key = "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"
+
+chat_history = []
+
+def generate_response(prompt):
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=chat_history,
+        temperature=0.2,
+        max_tokens=250
+    )
+    message = response.choices[0]["message"]['content']
+    return message
+
+while True:
+    user_input = input("You: ")
+    chat_history.append({"role": "user", "content": user_input})
+    # if user_input.lower() == "bye":
+    #     print("ChatGPT: Goodbye!")
+    #     break
+    prompt = f"You: {user_input}\nChatGPT:"
+    response = generate_response(chat_history)
+    chat_history.append({"role": "assistant", "content": response})
+    print(f"ChatGPT: {response}")
+
