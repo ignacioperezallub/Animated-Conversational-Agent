1bc2b77d703cc32b93298c549726df0deb789040
Andres Mesa <a1mesama@enib.fr>
2023-04-05 10:45:09 +0200

    Fork to gitlab

--- reco_vocal.py
+++ reco_vocal.py
@@ -11,11 +11,10 @@ if __name__ == '__main__':
         recognizer.dynamic_energy_threshold = False
         recognizer.adjust_for_ambient_noise(source)
         print(recognizer.energy_threshold)
-        print(recognizer.pause_threshold)
-        input("Enter to start recording")
+        input("Press enter to start recording")
         i = 0
         j = 0
-        min_speech_time = 0.5
+        min_speech_time = 1
         while True:        
             try:
                 audio = recognizer.listen(source, timeout=1)
@@ -23,10 +22,16 @@ if __name__ == '__main__':
                 speech_time = int(len(audio.frame_data))/(audio.sample_width*audio.sample_rate)
                 print(speech_time)
                 if(speech_time > min_speech_time):
-                    print("Min speech length achieved")
-                    with open("microphone-results_"+str(i)+".wav", "wb") as f:
-                        f.write(audio.get_wav_data())
-                        i += 1
+                    # recognize speech using Google Speech Recognition
+                    """try:
+                        # for testing purposes, we're just using the default API key
+                        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+                        # instead of `r.recognize_google(audio)`
+                        print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+                    except sr.UnknownValueError:
+                        print("Google Speech Recognition could not understand audio")
+                    except sr.RequestError as e:
+                        print("Could not request results from Google Speech Recognition service; {0}".format(e))"""
             except sr.WaitTimeoutError:
                 print("silence " + str(j))
                 j += 1
